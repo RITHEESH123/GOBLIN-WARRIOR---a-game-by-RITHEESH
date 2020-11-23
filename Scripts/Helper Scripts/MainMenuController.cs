ce27d7938117b88d8db9da1f40f96e343e602165
RITHEESH R <71439671+RITHEESH123@users.noreply.github.com>
2020-11-23 15:27:59 +0530

    SCRIPTS COMMIT
    I have attached all the C# scripts that I have coded to make this game

---
+++ Scripts/Helper Scripts/MainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Animator levelPanelAnim;
+
+    public void PlayGame()
+    {
+        levelPanelAnim.Play("SlideIn");
+    }
+    public void Back()
+    {
+        levelPanelAnim.Play("SlideOut");
+    }
+}
---
+++ Scripts/Player Scripts/PlayerHealth.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health = 100;
+    private PlayerScript playerScript;
+
+    private Animator anim;
+
+    void Awake()
+    {
+        playerScript = GetComponent<PlayerScript>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        GameplayController.instance.DisplayHealth(health);
+    }
+
+    public void ApplyDamage(int damageAmount)
+    {
+        health -= damageAmount;
+
+        if (health < 0)
+        {
+            health = 0;
+        }
+
+        //Display the Health Value
+        GameplayController.instance.DisplayHealth(health);
+
+        if (health == 0)
+        {
+            playerScript.enabled = false;
+            anim.Play(MyTags.DEAD_ANIMATION);
+
+            //CALL THE GAMEOVER
+            GameplayController.instance.isPlayerAlive = false;
+
+            // GAMEOVER PANEL
+            GameplayController.instance.GameOver();
+
+        }
+    }
+
+    void OnTriggerEnter(Collider target)
+    {
+        if (target.tag == MyTags.COIN_TAG)
+        {
+            target.gameObject.SetActive(false);
+            GameplayController.instance.CoinCollected();
+            SoundManager.instance.PlayCoinSound();
+        }
+    }
+
+}
